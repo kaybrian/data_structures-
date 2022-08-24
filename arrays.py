52c5d56f8f8c830fc43ae4d2022d757f3cc36c5a
kaybrian <kayonbrian@gmail.com>
2022-08-24 20:50:44 +0200

    creating or learnign data structures and algorithms

---
+++ arrays.py
@@ -0,0 +1,21 @@
+new_list = [1, 2, 3]
+result = new_list[0]
+
+# searching for an element in arrays
+if 1 in new_list:
+    print(True)
+
+
+for n in new_list:
+    if n == 1:
+        print(True)
+    break
+
+# inserting values in an array
+numbers = []
+numbers.extend([4, 5, 6, 7, 8])
+
+print(numbers)
+
+
+# Building our own data structure (linked lists)
